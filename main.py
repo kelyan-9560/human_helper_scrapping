7308ac0e0b830b05b3036053db7a0b70aa7256bd
kelyan-9560 <kelyan.bervin@gmail.com>
2021-08-25 21:41:31 +0100

    recuperation de la meteo en fonction de la ville

--- main.py
+++ main.py
@@ -1,17 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
 
-#url = 'http://localhost:4200/camps'
-url = 'https://meteo.francetvinfo.fr/france/ile-de-france/bobigny_93000'
-response = requests.get(url)
 
-if response.ok:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    tempSoiree = soup.find('ul', {'class': 'forecast-details'}).find('li', {'class': 'evening'}).find('div', {'class': 'forecast big'}).find('span', {'class': 'temparature'})
-
-    print(tempSoiree.)
+# url = 'http://localhost:4200/camps'
+# url = 'https://meteo.francetvinfo.fr/france/ile-de-france/bobigny_93000'
 
 
-else:
-    print(response)
+def print_data(datas, hour, temp):
+    if hour is not None :
+        if temp is not None:
+            datas[hour] = temp
 
+
+
+def write_in_file(filename, datas):
+    with open(filename, 'w') as file:
+        for key, value in datas.items():
+            file.write('[' + key.text + ':' + value.text + ']' + '\n')
+
+
+
+
+def meteo(ville):
+    ville.lower()
+
+    url = 'https://www.xn--mto-bmab.fr/' + ville
+    response = requests.get(url)
+
+    datas = {}
+
+    if response.ok:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        liS = soup.findAll('li')
+        tabHours = []
+        tabTemps = []
+        for li in liS:
+            hour = li.find('h4')
+            tabHours.append(hour)
+
+            temp = li.find('dd', {'class': 'temp'})
+            tabTemps.append(temp)
+
+            print_data(datas, hour, temp)
+
+        write_in_file('data.txt', datas)
+
+        print(tabHours)
+        print(tabTemps)
+        print(datas)
+
+    else:
+        print(response)
+
+
+test = 'ermont'
+meteo(test)
